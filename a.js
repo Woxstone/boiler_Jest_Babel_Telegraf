62b6454dad42c228a6751e8381cb815f1593f44b
Woxstone <nachomerling@gmail.com>
2022-02-25 10:51:03 +0100

    limpieza

--- a.js
+++ a.js
@@ -1,9 +1,4 @@
 import fs from 'fs';
-// import 'dotenv/config';
-
-
-// const filepath = './a.json';
-// const data = [24];
 
 function read(filename) {
     try {
@@ -11,23 +6,8 @@ function read(filename) {
         console.log(info)
         return info;
     } catch (err) {
-        // console.log(err);
         return false;
     }
 };
-
-// function write(filename, data) {
-//     return 'cosas'
-//     try {
-//         fs.writeFileSync(filename, JSON.stringify(data));
-//         return true;
-//     } catch (err) {
-//         console.log(err);
-//         return false;
-//     }
-// }
-
-// console.log('write', write(filepath));
-// console.log('read' ,read(filepath));
 
 export { read };
--- b.js
+++ b.js
@@ -1,15 +1,10 @@
 import fs from 'fs';
 
-const filepath = './a.json';
-const data = [24];
-
 function write(filename, data) {
-    // return 'cosas'
     try {
         fs.writeFileSync(filename, JSON.stringify(data));
         return true;
     } catch (err) {
-        // console.log(err);
         return false;
     }
 }
